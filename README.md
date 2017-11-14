a022c1b830703c5e02dd60aa5d224c0ac3a21f00
cosmic-cortex <theodore.danka@gmail.com>
2017-11-14 19:22:45 +0100

    pool/skeleton implemented for pool-based learners

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ModAL
 Modular Active Learning framework for Python3
+
+## Introduction
+ModAL is a Pythonic active learning framework, designed with *modularity, flexibility* and *extensibility* in mind.
---
+++ pool/model.py
@@ -0,0 +1,17 @@
+class ActiveLearner:
+	def __init__(self):
+		pass
+
+	def calculate_utility(self):
+		pass
+
+	def query(self):
+		pass
+
+
+class Committee:
+	def __init__(self):
+		pass
+
+	def vote(self):
+		pass
