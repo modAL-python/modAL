8062c7fe8e26578537e4ecc06a39d28856ebbf93
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-11-15 11:05:34 +0100

    utilities/uncertainty() model argument renamed to predictor

--- active_learning/utilities.py
+++ active_learning/utilities.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def uncertainty(model, data):
-	uncertainties = model.score(data)
+def uncertainty(predictor, data):
+	uncertainties = predictor.score(data)
 	max_uncertainty_idx = np.argmax(uncertainties)
 	return max_uncertainty_idx, data[max_uncertainty_idx]
