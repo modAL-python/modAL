7e0d788c83d4beccc882c019e71fa55806aea032
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-11-15 13:13:00 +0100

    1. models/ActiveLearner now possible to initialize with training data 2. models/ActiveLearner.fit_to_known() method implemented

--- active_learning/models.py
+++ active_learning/models.py
@@ -1,11 +1,18 @@
 class ActiveLearner:
-	def __init__(self, predictor, utility_function):
+	def __init__(
+			self,
+			predictor, utility_function, 					# building blocks of the learner
+			initial_data=None, initial_labels=None			# initial data if available
+	):
 		assert callable(utility_function), 'utility_function must be callable'
 
 		self.predictor = predictor
 		self.utility_function = utility_function
-		self.training_data = None
-		self.training_labels = None
+		self.training_data = initial_data
+		self.training_labels = initial_labels
+
+		if (type(initial_data) != type(None)) and (type(initial_labels) != type(None)):
+			self.fit_to_known()
 
 	def calculate_utility(self, data):
 		return self.utility_function(self.predictor, data)
@@ -13,12 +20,15 @@ class ActiveLearner:
 	def query(self):
 		pass
 
-	def fit(self, observation, label):
-		pass
+	def fit_to_known(self):
+		self.predictor.fit(self.training_data, self.training_labels)
 
 	def add_training_data(self, new_data, new_label):
 		# TODO: get rid of the if clause
 		# TODO: test if this works with multiple shapes and types of data
+
+		assert len(new_data) == len(new_label), 'the number of new data points and number of labels must match'
+
 		if type(training_data) != type(None):
 			try:
 				self.training_data = np.vstack((self.training_data, new_data))
