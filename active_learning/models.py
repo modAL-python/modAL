8fa11142ad914d3335342f78297fa89bb76928b6
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-11-15 12:50:11 +0100

    models/ActiveLearner.add_training_data() implemented

--- active_learning/models.py
+++ active_learning/models.py
@@ -8,7 +8,7 @@ class ActiveLearner:
 		self.training_labels = None
 
 	def calculate_utility(self, data):
-		return utility_function(self.predictor, data)
+		return self.utility_function(self.predictor, data)
 
 	def query(self):
 		pass
@@ -16,8 +16,20 @@ class ActiveLearner:
 	def fit(self, observation, label):
 		pass
 
-	def give_labels(self):
-		pass
+	def add_training_data(self, new_data, new_label):
+		# TODO: get rid of the if clause
+		# TODO: test if this works with multiple shapes and types of data
+		if type(training_data) != type(None):
+			try:
+				self.training_data = np.vstack((self.training_data, new_data))
+				self.training_labels = np.vstack((self.training_labels, new_label))
+			except ValueError:
+				raise ValueError('the dimensions of the new training data and label must'
+								 'agree with the training data and labels provided so far')
+
+		else:
+			self.training_data = new_data
+			self.training_labels = new_label
 
 
 class Committee:
--- examples/shape_learning/shape_learning.py
+++ examples/shape_learning/shape_learning.py
@@ -7,10 +7,14 @@ the scikit-learn implementation of the Random Forest Classifier algorithm
 
 
 from scipy.misc import imread
-from ModAL.active_learning.models import ActiveLearner
 from sklearn.ensemble import RandomForestClassifier
+from ModAL.active_learning.models import ActiveLearner
+from ModAL.active_learning.utilities import uncertainty
 
 # import the image
 data = imread('square.png')
 # reshape the data
 data = data.reshape(-1,)
+
+# create an ActiveLearner instance
+learner = ActiveLearner(predictor=RandomForestClassifier(), utility_function=uncertainty)
