576aadd262af00609b2a940077c56c71ff6ee90e
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-11-15 14:04:42 +0100

    examples/shape_learning grid construction from image implemented

--- examples/shape_learning/shape_learning.py
+++ examples/shape_learning/shape_learning.py
@@ -5,7 +5,7 @@ In this example, we will demonstrate the use of ActiveLearner with
 the scikit-learn implementation of the Random Forest Classifier algorithm
 """
 
-
+import numpy as np
 from scipy.misc import imread
 from sklearn.ensemble import RandomForestClassifier
 from ModAL.active_learning.models import ActiveLearner
@@ -13,8 +13,13 @@ from ModAL.active_learning.utilities import uncertainty
 
 # import the image
 data = imread('square.png')
-# reshape the data
-data = data.reshape(-1,)
+# create the data from the image
+X = np.transpose(
+	[np.tile(np.asarray(range(data.shape[0])), data.shape[1]),
+	 np.repeat(np.asarray(range(data.shape[1])), data.shape[0])]
+)
 
 # create an ActiveLearner instance
-learner = ActiveLearner(predictor=RandomForestClassifier(), utility_function=uncertainty)
+learner = ActiveLearner(
+	predictor=RandomForestClassifier(), utility_function=uncertainty
+)
