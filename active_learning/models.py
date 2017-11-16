1a5cd903c675be247803e0c49330981c947bff17
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-11-16 13:31:25 +0100

    models/ActiveLearner docstrings added and some variable names refactored

--- active_learning/models.py
+++ active_learning/models.py
@@ -1,24 +1,51 @@
+import numpy as np
+from sklearn.utils import check_array
+
 class ActiveLearner:
+	"""
+	This class is an abstract model of a general active learning algorithm.
+	"""
 	def __init__(
 			self,
 			predictor, utility_function, 					# building blocks of the learner
-			initial_data=None, initial_labels=None			# initial data if available
+			training_data=None, training_labels=None			# initial data if available
 	):
+		"""
+		:param predictor: an instance of the predictor
+		:param utility_function: function to calculate utilities
+		:param training_data: initial training data if available
+		:param training_labels: labels corresponding to the initial training data
+		"""
 		assert callable(utility_function), 'utility_function must be callable'
 
 		self.predictor = predictor
 		self.utility_function = utility_function
-		self.training_data = initial_data
-		self.training_labels = initial_labels
+		self.training_data = training_data
+		self.training_labels = training_labels
 
-		if (type(initial_data) != type(None)) and (type(initial_labels) != type(None)):
+		if (type(training_data) != type(None)) and (type(training_labels) != type(None)):
 			self.fit_to_known()
 
 	def calculate_utility(self, data):
+		"""
+		This method calls the utility function provided for ActiveLearner
+		on the data passed to it. It is used to measure utilities for each
+		data point.
+		:param data: data points for which the utilities should be measures
+		:return: utility values for each datapoint
+		"""
 		return self.utility_function(self.predictor, data)
 
-	def query(self):
-		pass
+	def query(self, data):
+		"""
+		Finds the most informative point in the data provided, then
+		returns the instance and its index
+		:param data:
+		:return:
+		"""
+		utilities = self.calculate_utility(data)
+		query_idx = np.argmax(utilities)
+		return query_idx, data[query_idx]
 
 	def fit_to_known(self):
 		"""
@@ -38,9 +65,17 @@ class ActiveLearner:
 		self.fit_to_known()
 
 	def add_training_data(self, new_data, new_label):
+		"""
+		Adds the new data and label to the known data, but does
+		not retrain the model.
+		:param new_data:
+		:param new_label:
+		:return:
+		"""
 		# TODO: get rid of the if clause
 		# TODO: test if this works with multiple shapes and types of data
 
+		new_data, new_label = check_array(new_data), check_array(new_label)
 		assert len(new_data) == len(new_label), 'the number of new data points and number of labels must match'
 
 		if type(training_data) != type(None):
@@ -54,6 +89,14 @@ class ActiveLearner:
 		else:
 			self.training_data = new_data
 			self.training_labels = new_label
+
+	def predict(self, data):
+		"""
+		Interface for the predictor
+		:param data:
+		:return:
+		"""
+		return self.predictor.predict
 
 
 class Committee:
