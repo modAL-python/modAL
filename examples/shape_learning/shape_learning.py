bc1dee9648fa214c11dc8cbdb6bd14f64c784f5f
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-11-16 13:32:45 +0100

    examples/shape_learning learner initialized

--- examples/shape_learning/shape_learning.py
+++ examples/shape_learning/shape_learning.py
@@ -6,20 +6,36 @@ the scikit-learn implementation of the Random Forest Classifier algorithm
 """
 
 import numpy as np
-from scipy.misc import imread
+from scipy import misc
 from sklearn.ensemble import RandomForestClassifier
 from ModAL.active_learning.models import ActiveLearner
-from ModAL.active_learning.utilities import uncertainty
+from ModAL.active_learning.utilities import classifier_uncertainty
 
 # import the image
-data = imread('square.png')
+data = misc.imread('square.png')
 # create the data from the image
 X = np.transpose(
 	[np.tile(np.asarray(range(data.shape[0])), data.shape[1]),
 	 np.repeat(np.asarray(range(data.shape[1])), data.shape[0])]
 )
+# map the intensity values against the grid
+y = np.asarray([data[P[0], P[1]] for P in X])
+
+# adding 10 points to the dataset
+initial_idx = np.random.choice(range(len(y)), size=10)
+X_train, y_train = X[initial_idx], y[initial_idx]
+# to make sure that the initial dataset contains at least two classes,
+# we repeat the above process until it is satisfied
+while len(np.unique(y_train)) == 1:
+	initial_idx = np.random.choice(range(len(y)), size=10)
+	X_train, y_train = X[initial_idx], y[initial_idx]
 
 # create an ActiveLearner instance
 learner = ActiveLearner(
-	predictor=RandomForestClassifier(), utility_function=uncertainty
+	predictor=RandomForestClassifier(), utility_function=classifier_uncertainty,
+	training_data=X_train, training_labels=y_train
 )
+
+
+
+
