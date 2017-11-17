9759decb96a28231332c9659cfc2e03ced5de67c
cosmic-cortex <28926195+cosmic-cortex@users.noreply.github.com>
2017-11-17 21:16:10 +0100

    examples/shape_learning Jupyter notebook added

--- active_learning/models.py
+++ active_learning/models.py
@@ -3,106 +3,109 @@ from sklearn.utils import check_array
 
 
 class ActiveLearner:
-	"""
-	This class is an abstract model of a general active learning algorithm.
-	"""
-	def __init__(
-			self,
-			predictor, utility_function, 					# building blocks of the learner
-			training_data=None, training_labels=None			# initial data if available
-	):
-		"""
-		:param predictor: an instance of the predictor
-		:param utility_function: function to calculate utilities
-		:param training_data: initial training data if available
-		:param training_labels: labels corresponding to the initial training data
-		"""
-		assert callable(utility_function), 'utility_function must be callable'
+    """
+    This class is an abstract model of a general active learning algorithm.
+    """
+    def __init__(
+            self,
+            predictor, utility_function, 					# building blocks of the learner
+            training_data=None, training_labels=None			# initial data if available
+    ):
+        """
+        :param predictor: an instance of the predictor
+        :param utility_function: function to calculate utilities
+        :param training_data: initial training data if available
+        :param training_labels: labels corresponding to the initial training data
+        """
+        assert callable(utility_function), 'utility_function must be callable'
 
-		self.predictor = predictor
-		self.utility_function = utility_function
-		self.training_data = check_array(training_data)
-		self.training_labels = check_array(training_labels, ensure_2d=False)
+        self.predictor = predictor
+        self.utility_function = utility_function
+        self.training_data = check_array(training_data)
+        self.training_labels = check_array(training_labels, ensure_2d=False)
 
-		if (type(training_data) != type(None)) and (type(training_labels) != type(None)):
-			self.fit_to_known()
+        if (type(training_data) != type(None)) and (type(training_labels) != type(None)):
+            self.fit_to_known()
 
-	def calculate_utility(self, data):
-		"""
-		This method calls the utility function provided for ActiveLearner
-		on the data passed to it. It is used to measure utilities for each
-		data point.
-		:param data: data points for which the utilities should be measures
-		:return: utility values for each datapoint
-		"""
-		return self.utility_function(self.predictor, data)
+    def calculate_utility(self, data):
+        """
+        This method calls the utility function provided for ActiveLearner
+        on the data passed to it. It is used to measure utilities for each
+        data point.
+        :param data: data points for which the utilities should be measures
+        :return: utility values for each datapoint
+        """
+        return self.utility_function(self.predictor, data)
 
-	def query(self, data):
-		"""
-		Finds the most informative point in the data provided, then
-		returns the instance and its index
-		:param data:
-		:return:
-		"""
-		utilities = self.calculate_utility(data)
-		query_idx = np.argmax(utilities)
-		return query_idx, data[query_idx]
+    def query(self, data):
+        """
+        Finds the most informative point in the data provided, then
+        returns the instance and its index
+        :param data:
+        :return:
+        """
+        utilities = self.calculate_utility(data)
+        query_idx = np.argmax(utilities)
+        return query_idx, data[query_idx]
 
-	def fit_to_known(self):
-		"""
-		This method fits self.predictor to the training data and labels
-		provided to it so far.
-		"""
-		self.predictor.fit(self.training_data, self.training_labels)
+    def fit_to_known(self):
+        """
+        This method fits self.predictor to the training data and labels
+        provided to it so far.
+        """
+        self.predictor.fit(self.training_data, self.training_labels)
 
-	def add_and_retrain(self, new_data, new_label):
-		"""
-		This function adds the given data to the training examples
-		and retrains the predictor with the augmented dataset
-		:param new_data: new training data
-		:param new_label: new training labels for the data
-		"""
-		self.add_training_data(new_data, new_label)
-		self.fit_to_known()
+    def add_and_retrain(self, new_data, new_label):
+        """
+        This function adds the given data to the training examples
+        and retrains the predictor with the augmented dataset
+        :param new_data: new training data
+        :param new_label: new training labels for the data
+        """
+        self.add_training_data(new_data, new_label)
+        self.fit_to_known()
 
-	def add_training_data(self, new_data, new_label):
-		"""
-		Adds the new data and label to the known data, but does
-		not retrain the model.
-		:param new_data:
-		:param new_label:
-		:return:
-		"""
-		# TODO: get rid of the if clause
-		# TODO: test if this works with multiple shapes and types of data
+    def add_training_data(self, new_data, new_label):
+        """
+        Adds the new data and label to the known data, but does
+        not retrain the model.
+        :param new_data:
+        :param new_label:
+        :return:
+        """
+        # TODO: get rid of the if clause
+        # TODO: test if this works with multiple shapes and types of data
 
-		new_data, new_label = check_array(new_data), check_array(new_label, ensure_2d=False)
-		assert len(new_data) == len(new_label), 'the number of new data points and number of labels must match'
+        new_data, new_label = check_array(new_data), check_array(new_label, ensure_2d=False)
+        assert len(new_data) == len(new_label), 'the number of new data points and number of labels must match'
 
-		if type(self.training_data) != type(None):
-			try:
-				self.training_data = np.vstack((self.training_data, new_data))
-				self.training_labels = np.concatenate((self.training_labels, new_label))
-			except ValueError:
-				raise ValueError('the dimensions of the new training data and label must'
-								 'agree with the training data and labels provided so far')
+        if type(self.training_data) != type(None):
+            try:
+                self.training_data = np.vstack((self.training_data, new_data))
+                self.training_labels = np.concatenate((self.training_labels, new_label))
+            except ValueError:
+                raise ValueError('the dimensions of the new training data and label must'
+                                 'agree with the training data and labels provided so far')
 
-		else:
-			self.training_data = new_data
-			self.training_labels = new_label
+        else:
+            self.training_data = new_data
+            self.training_labels = new_label
 
-	def predict(self, data):
-		"""
-		Interface for the predictor
-		:param data:
-		:return:
-		"""
-		return self.predictor.predict
+    def predict(self, data):
+        """
+        Interface for the predictor
+        :param data:
+        :return:
+        """
+        return self.predictor.predict(data)
+
+    def predict_proba(self, data):
+        return self.predictor.predict_proba(data)
 
 
 class Committee:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def vote(self):
-		pass
+    def vote(self):
+        pass
--- examples/shape_learning/shape_learning.py
+++ examples/shape_learning/shape_learning.py
@@ -8,15 +8,20 @@ the scikit-learn implementation of the Random Forest Classifier algorithm
 import numpy as np
 from scipy import misc
 from sklearn.ensemble import RandomForestClassifier
-from ModAL.active_learning.models import ActiveLearner
-from ModAL.active_learning.utilities import classifier_uncertainty
+from modAL.active_learning.models import ActiveLearner
+from modAL.active_learning.utilities import classifier_uncertainty
 
-# import the image
-data = misc.imread('square.png')
+# creating the image
+im_width = 200
+im_height = 200
+data = np.zeros((im_height, im_width))
+data[50:199 - 50, 50:199 - 50] = 1
+misc.imshow(data)
+
 # create the data from the image
 X = np.transpose(
-	[np.tile(np.asarray(range(data.shape[0])), data.shape[1]),
-	 np.repeat(np.asarray(range(data.shape[1])), data.shape[0])]
+    [np.tile(np.asarray(range(data.shape[0])), data.shape[1]),
+     np.repeat(np.asarray(range(data.shape[1])), data.shape[0])]
 )
 # map the intensity values against the grid
 y = np.asarray([data[P[0], P[1]] for P in X])
@@ -27,17 +32,17 @@ X_train, y_train = X[initial_idx], y[initial_idx]
 # to make sure that the initial dataset contains at least two classes,
 # we repeat the above process until it is satisfied
 while len(np.unique(y_train)) == 1:
-	initial_idx = np.random.choice(range(len(y)), size=10)
-	X_train, y_train = X[initial_idx], y[initial_idx]
+    initial_idx = np.random.choice(range(len(y)), size=10)
+    X_train, y_train = X[initial_idx], y[initial_idx]
 
 # create an ActiveLearner instance
 learner = ActiveLearner(
-	predictor=RandomForestClassifier(), utility_function=classifier_uncertainty,
-	training_data=X_train, training_labels=y_train
+    predictor=RandomForestClassifier(), utility_function=classifier_uncertainty,
+    training_data=X_train, training_labels=y_train
 )
 
 for round_idx in range(20):
-	query_idx, query_inst = learner.query(X)
-	learner.add_and_retrain(X[query_idx].reshape(1, -1), y[query_idx].reshape(-1, ))
+    query_idx, query_inst = learner.query(X)
+    learner.add_and_retrain(X[query_idx].reshape(1, -1), y[query_idx].reshape(-1, ))
 
-misc.imshow(learner.predictor.predict(X).reshape(100, 100))
+misc.imshow(learner.predict(X).reshape(im_height, im_width))
