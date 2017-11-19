92c7219daf41417738f7e7e6771dcb7add0d6977
cosmic-cortex <28926195+cosmic-cortex@users.noreply.github.com>
2017-11-19 10:24:02 +0100

    tests.utilities_test classifier_margin() test implemented

--- tests/utilities_test.py
+++ tests/utilities_test.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
+import modAL.utilities
 from collections import namedtuple
-from modAL.active_learning.utilities import classifier_uncertainty
 
 
 Test = namedtuple('Test', ['input', 'output'])
@@ -19,13 +19,25 @@ class TestUtilities(unittest.TestCase):
 
     def test_uncertainty(self):
         test_cases = (Test(p * np.ones(shape=(k + 1, l + 1)), (1 - p) * np.ones(shape=(k + 1, )))
-                      for k in range(1000) for l in range(10) for p in np.linspace(0, 1, 10))
+                      for k in range(100) for l in range(10) for p in np.linspace(0, 1, 10))
         for case in test_cases:
             mock_classifier = MockClassifier(case.input)
             np.testing.assert_almost_equal(
-                classifier_uncertainty(mock_classifier, np.random.rand(10)),
-                case.output)
+                modAL.utilities.classifier_uncertainty(mock_classifier, np.random.rand(10)),
+                case.output
+            )
+
+    def test_margin(self):
+        test_cases = (Test(p * np.ones(shape=(k + 1, l + 1)), np.zeros(shape=(k + 1,)))
+                      for k in range(100) for l in range(10) for p in np.linspace(0, 1, 10))
+        for case in test_cases:
+            mock_classifier = MockClassifier(case.input)
+            np.testing.assert_almost_equal(
+                modAL.utilities.classifier_margin(mock_classifier, np.random.rand(10)),
+                case.output
+            )
 
 
 if __name__ == '__main__':
+
     unittest.main()
