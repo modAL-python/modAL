f504d697729b1ef5b6b51a86669cb9c317b00eda
cosmic-cortex <28926195+cosmic-cortex@users.noreply.github.com>
2017-11-19 14:21:13 +0100

    modAL.utilities.classifier_max_margin() now uses modAL.utils.np_utils.max_margin()

--- modAL/utilities.py
+++ modAL/utilities.py
@@ -1,4 +1,5 @@
 import numpy as np
+from modAL.utils.np_utils import max_margin
 
 
 def classifier_uncertainty(classifier, data):
@@ -10,22 +11,4 @@ def classifier_uncertainty(classifier, data):
 
 
 def classifier_margin(classifier, data):
-    classwise_uncertainty = classifier.predict_proba(data)
-
-    margins = np.zeros(shape=(classwise_uncertainty.shape[0], ))
-
-    for point_idx, point_uncertainty_dist in enumerate(classwise_uncertainty):
-        first_max = -np.inf
-        second_max = -np.inf
-
-        for val in point_uncertainty_dist:
-            if val > first_max:
-                second_max = first_max
-                first_max = val
-
-        if second_max != -np.inf:
-            margins[point_idx] = first_max - second_max
-        else:
-            margins[point_idx] = 0.0
-
-    return margins
+    return max_margin(classifier.predict_proba(data))
