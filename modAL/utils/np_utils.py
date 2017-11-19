6dc96700f6670f34e647a28e96a32b786757322b
cosmic-cortex <28926195+cosmic-cortex@users.noreply.github.com>
2017-11-19 14:20:31 +0100

    modAL.utils.np_utils.max_margin() implemented

--- modAL/utils/np_utils.py
+++ modAL/utils/np_utils.py
@@ -1,10 +1,30 @@
 import numpy as np
+from sklearn.utils import check_array
 
 
-def max_margin(array):
+def max_margin(array, axis=None):
     """
     Calculates the difference between the first and second largest element of an array.
     :param array: numpy.ndarray
+    :param axis: axis along which to calculate the max margin
     :return: max margin of array or
     """
-    pass
+    check_array(array, ensure_2d=True)
+
+    margins = np.zeros(shape=(array.shape[0],))
+
+    for elem_idx, elem in enumerate(array):
+        first_max = -np.inf
+        second_max = -np.inf
+
+        for val in elem:
+            if val > first_max:
+                second_max = first_max
+                first_max = val
+
+        if second_max != -np.inf:
+            margins[elem_idx] = first_max - second_max
+        else:
+            margins[elem_idx] = 0.0
+
+    return margins
