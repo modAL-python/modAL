545c8da072354bde675ef6b9985e9317f1d2de6f
cosmic-cortex <28926195+cosmic-cortex@users.noreply.github.com>
2017-11-19 10:24:39 +0100

    directory structure refactored

---
+++ modAL/utils/np_utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def max_margin(array):
+    """
+    Calculates the difference between the first and second largest element of an array.
+    :param array: numpy.ndarray
+    :return: max margin of array or
+    """
+    pass
