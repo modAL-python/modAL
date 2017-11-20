f95a3df2061f368d612ad4868738eddbde6263ad
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-11-20 12:05:13 +0100

    tests.core_tests.MockClassifier.calculate_utility() mock method implemented

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -13,16 +13,22 @@ class MockClassifier:
     Mock classifier object for testing. The predict_proba method returns the
     object given for argument predict_proba_return.
     """
-    def __init__(self, predict_proba_return):
+    def __init__(self, predict_proba_return=None, calculate_utility_return=None):
         self.predict_proba_return = predict_proba_return
+        self.calculate_utility_return = calculate_utility_return
 
     def predict_proba(self, data):
         return self.predict_proba_return
+
+    def calculate_utility(self, data):
+        return self.calculate_utility_return
 
 
 class TestUtilities(unittest.TestCase):
 
     def test_uncertainty(self):
+        print('Testing modAL.utilities.classifier_uncertainty()...')
+
         test_cases = (Test(p * np.ones(shape=(k + 1, l + 1)), (1 - p) * np.ones(shape=(k + 1, )))
                       for k in range(100) for l in range(10) for p in np.linspace(0, 1, 10))
         for case in test_cases:
@@ -33,6 +39,8 @@ class TestUtilities(unittest.TestCase):
             )
 
     def test_margin(self):
+        print('Testing modAL.utilities.classifier_margin()...')
+
         test_cases_1 = (Test(p * np.ones(shape=(k + 1, l + 1)), np.zeros(shape=(k + 1,)))
                       for k in range(100) for l in range(10) for p in np.linspace(0, 1, 10))
         test_cases_2 = (Test(p * np.tile(np.asarray(range(k+1))+1.0, l+1).reshape(l+1, k+1),
@@ -45,6 +53,11 @@ class TestUtilities(unittest.TestCase):
                 case.output
             )
 
+
+class TestCommittee(unittest.TestCase):
+
+    def test_calculate_utility(self):
+        print('Testing Committee.calculate_utility()...')
 
 if __name__ == '__main__':
     unittest.main()
