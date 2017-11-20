873136b82dd233631e91d7e92df04ae1f5da34ba
cosmic-cortex <28926195+cosmic-cortex@users.noreply.github.com>
2017-11-20 21:27:11 +0100

    modAL.utils.np_utils.max_margin() test implemented for np.ndarrays and np.ma.masked_arrays

--- modAL/utils/np_utils.py
+++ modAL/utils/np_utils.py
@@ -6,7 +6,7 @@ def max_margin(array):
     """
     Calculates the difference between the first and second largest element of an array.
     :param array: numpy.ndarray
-    :return: max margin of array or
+    :return: max margin of array
     """
     check_array(array, ensure_2d=True)
 
