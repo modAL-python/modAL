1ed9b2c2d9fbb0980d3f9f8647429f0dbd512fd8
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-11-21 10:02:12 +0100

    modAL.utils.evaluation.Evaluator.__init__() implemented

--- modAL/utils/evaluation.py
+++ modAL/utils/evaluation.py
@@ -2,3 +2,29 @@
 Functions for evaluating the performance of the learners
 and comparing them to each other
 """
+
+
+class Evaluator:
+
+    def __init__(
+            self, learner,
+            X_train, y_train,
+            X_test, y_test,
+            replace=True
+    ):
+        # model and testing parameters
+        self.learner = learner
+        self.replace = replace
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+
+        # model performance
+        self.performance = list()
+
+    def run_training(self, replace=True):
+        pass
+
+    def _measure_performance(self):
+        pass
