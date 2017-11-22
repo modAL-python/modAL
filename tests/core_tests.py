0e654e33287738732505cc294507ae11b498412a
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-11-22 10:21:03 +0100

    tests.core_tests.MockClassifier moved to tests.mock

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -4,31 +4,10 @@ import modAL.utilities
 import modAL.models
 from itertools import chain
 from collections import namedtuple
+from mock import MockClassifier
 
 
 Test = namedtuple('Test', ['input', 'output'])
-
-
-class MockClassifier:
-    """
-    Mock classifier object for testing. The predict_proba method returns the
-    object given for argument predict_proba_return.
-    """
-    def __init__(
-            self, predict_proba_return=None, calculate_utility_return=None, predict_return=None
-    ):
-        self.predict_proba_return = predict_proba_return
-        self.calculate_utility_return = calculate_utility_return
-        self.predict_return = None
-
-    def predict_proba(self, data):
-        return self.predict_proba_return
-
-    def predict(self, data):
-        return self.predict_return
-
-    def calculate_utility(self, data):
-        return self.calculate_utility_return
 
 
 class TestUtilities(unittest.TestCase):
