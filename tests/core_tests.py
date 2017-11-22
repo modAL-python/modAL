605fba160a6e9dea6f4dfd3cd919b671b6b1a712
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-11-22 10:18:12 +0100

    tests.core_tests.MockClassifier predict() method added

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -14,12 +14,18 @@ class MockClassifier:
     Mock classifier object for testing. The predict_proba method returns the
     object given for argument predict_proba_return.
     """
-    def __init__(self, predict_proba_return=None, calculate_utility_return=None):
+    def __init__(
+            self, predict_proba_return=None, calculate_utility_return=None, predict_return=None
+    ):
         self.predict_proba_return = predict_proba_return
         self.calculate_utility_return = calculate_utility_return
+        self.predict_return = None
 
     def predict_proba(self, data):
         return self.predict_proba_return
+
+    def predict(self, data):
+        return self.predict_return
 
     def calculate_utility(self, data):
         return self.calculate_utility_return
