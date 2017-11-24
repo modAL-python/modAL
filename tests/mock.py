b1eb1470625513263887b37c9c6597c54582fe66
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-11-24 10:50:04 +0100

    tests.mock MockUtility class implemented

--- tests/mock.py
+++ tests/mock.py
@@ -1,20 +1,33 @@
+class MockUtility:
+    def __init__(self, utility_return):
+        self.utility_return = utility_return
+
+    def __call__(self, *args):
+        return self.utility_return
+
+
 class MockClassifier:
     """
     Mock classifier object for testing. The predict_proba method returns the
     object given for argument predict_proba_return.
     """
     def __init__(
-            self, predict_proba_return=None, calculate_utility_return=None, predict_return=None
+            self, predict_proba_return=None, calculate_utility_return=None, predict_return=None,
+            score_return=None
     ):
-        self.predict_proba_return = predict_proba_return
         self.calculate_utility_return = calculate_utility_return
-        self.predict_return = None
+        self.predict_return = predict_return
+        self.predict_proba_return = predict_proba_return
+        self.score_return = score_return
 
-    def predict_proba(self, data):
-        return self.predict_proba_return
+    def calculate_utility(self, data):
+        return self.calculate_utility_return
 
     def predict(self, data):
         return self.predict_return
 
-    def calculate_utility(self, data):
-        return self.calculate_utility_return
+    def predict_proba(self, data):
+        return self.predict_proba_return
+
+    def score(self, data):
+        return self.score_return
