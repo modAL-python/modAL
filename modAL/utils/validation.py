7651d995685f8d92c5a9d8ed2dbb0a5ff124ee1d
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-12-06 13:05:15 +0100

    modAL.utils.validation.check_class_labels() return value changed

--- modAL/utils/validation.py
+++ modAL/utils/validation.py
@@ -1,14 +1,16 @@
 import numpy as np
 
+
 def check_class_labels(*args):
     """
     Checks the known class labels for each classifier.
     :param args: sklearn classifiers
-    :return: bool, True if the class labels are the same for each predictor, false otherwise
+    :return: int or bool, False if the class labels are not the same for each predictor
+                          number of classes if class labels are the same for each predictor
     """
 
     for classifier_idx in range(len(args) - 1):
         if not np.array_equal(args[classifier_idx].classes_, args[classifier_idx+1].classes_):
             return False
 
-    return True
+    return len(args[0].classes_)
