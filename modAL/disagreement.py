c1ea0700bd0364be4a8dbf2950fe10fa8a9edc03
cosmic-cortex <28926195+cosmic-cortex@users.noreply.github.com>
2017-12-10 17:47:11 +0100

    modAL.disagreement.vote_entropy() implementation started

--- modAL/disagreement.py
+++ modAL/disagreement.py
@@ -1,1 +1,13 @@
+"""
+Disagreement measures for the Committee model.
+"""
+
 import numpy as np
+from collections import Counter
+
+
+def vote_entropy(committee, data, **predict_proba_kwargs):
+    check_array(data)
+
+    vote = committee.predict(data, **predict_proba_kwargs)
+    vote_proba = np.zeros(shape=())
