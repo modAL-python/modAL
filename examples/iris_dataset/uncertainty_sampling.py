7ac63174271796a449649cb3b2136f164f177a46
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-12-13 16:24:03 +0100

    examples/iris_dataset/uncertainty_sampling.py MLPClassifier replaced with RandomForestClassifier; learner accuracy score plotted on final result

--- examples/iris_dataset/uncertainty_sampling.py
+++ examples/iris_dataset/uncertainty_sampling.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.datasets import load_iris
-from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 from modAL.models import ActiveLearner
 
 # loading the iris dataset
@@ -30,18 +30,18 @@ pool_data = np.delete(iris['data'], train_idx, axis=0)
 pool_labels = np.delete(iris['target'], train_idx)
 
 # active learning
-mlp_classifier = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
+rfc = RandomForestClassifier()
 learner = ActiveLearner(
-    predictor=mlp_classifier,
+    predictor=rfc,
     training_data=X_train, training_labels=y_train
 )
 
 n_queries = 20
 for idx in range(n_queries):
     query_idx, query_instance = learner.query(pool_data)
-    learner.add_and_retrain(
+    learner.teach(
         new_data=pool_data[query_idx].reshape(1, -1),
-        new_label=pool_labels[query_idx].reshape(-1, )
+        new_label=pool_labels[query_idx].reshape(1, )
     )
     # remove queried instance from pool
     pool_data = np.delete(pool_data, query_idx, axis=0)
@@ -50,5 +50,5 @@ for idx in range(n_queries):
 with plt.style.context('seaborn-white'):
     prediction = learner.predict(iris['data'])
     plt.scatter(x=pca[:, 0], y=pca[:, 1], c=prediction, cmap='viridis')
-    plt.title('Predictions after %i queries' % n_queries)
+    plt.title('Classification accuracy after %i queries: %f' % (n_queries, learner.score(iris['data'], iris['target'])))
     plt.show()
