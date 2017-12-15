9715a4c34b5d90e6801b82ba5c2cc28cda20ce60
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2017-12-15 12:42:35 +0100

    modAL.disagreement.vote_entropy() docstring added

--- modAL/disagreement.py
+++ modAL/disagreement.py
@@ -7,11 +7,33 @@ from collections import Counter
 from scipy.stats import entropy
 
 
-def vote_entropy(committee, data, **predict_proba_kwargs):
+def vote_entropy(committee, X, **predict_proba_kwargs):
+    """
+    Calculates the vote entropy for the Committee. First it computes the
+    predictions of X for each learner in the Committee, then calculates
+    the probability distribution of the votes. The entropy of this distribution
+    is the vote entropy of the Committee, which is returned.
+
+    Parameters
+    ----------
+    committee: modAL.models.Committee object
+        The Committee instance for which the vote entropy is to be calculated.
+
+    X: numpy.ndarray of shape (n_samples, n_features)
+        The data for which the vote entropy is to be calculated.
+
+    predict_proba_kwargs: keyword arguments
+        Keyword arguments for the predict_proba method of the Committee.
+
+    Returns
+    -------
+    entr: numpy.ndarray of shape (n_samples, )
+        Vote entropy of the Committee for the samples in X.
+    """
     n_learners = len(committee)
-    votes = committee.vote(data, **predict_proba_kwargs)
-    vote_proba = np.zeros(shape=(data.shape[0], len(committee.classes_)))
-    entr = np.zeros(shape=(data.shape[0], ))
+    votes = committee.vote(X, **predict_proba_kwargs)
+    vote_proba = np.zeros(shape=(X.shape[0], len(committee.classes_)))
+    entr = np.zeros(shape=(X.shape[0],))
 
     for vote_idx, vote in enumerate(votes):
         vote_counter = Counter(vote)
