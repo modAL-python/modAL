c5ebda79cb8d4a793ebfc2427091e73fb89a000e
cosmic-cortex <28926195+cosmic-cortex@users.noreply.github.com>
2017-12-17 09:15:51 +0100

    tests/core_tests.py TestUncertainties test_margin_sampling and test_entropy_sampling implemented

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -88,10 +88,31 @@ class TestUncertainties(unittest.TestCase):
                     np.testing.assert_array_equal(query_idx, true_query_idx)
 
     def test_margin_sampling(self):
-        pass
+        for n_samples in range(1, 10):
+            for n_classes in range(1, 10):
+                max_proba = np.zeros(n_classes)
+                for true_query_idx in range(n_samples):
+                    predict_proba = np.random.rand(n_samples, n_classes)
+                    predict_proba[true_query_idx] = max_proba
+                    classifier = mock.MockClassifier(predict_proba_return=predict_proba)
+                    query_idx, query_instance = modAL.uncertainty.uncertainty_sampling(
+                        classifier, np.random.rand(n_samples, n_classes)
+                    )
+                    np.testing.assert_array_equal(query_idx, true_query_idx)
 
     def test_entropy_sampling(self):
-        pass
+        for n_samples in range(1, 10):
+            for n_classes in range(2, 10):
+                max_proba = np.ones(n_classes)/n_classes
+                for true_query_idx in range(n_samples):
+                    predict_proba = np.zeros(shape=(n_samples, n_classes))
+                    predict_proba[:, 0] = 1.0
+                    predict_proba[true_query_idx] = max_proba
+                    classifier = mock.MockClassifier(predict_proba_return=predict_proba)
+                    query_idx, query_instance = modAL.uncertainty.uncertainty_sampling(
+                        classifier, np.random.rand(n_samples, n_classes)
+                    )
+                    np.testing.assert_array_equal(query_idx, true_query_idx)
 
 
 class TestQueries(unittest.TestCase):
