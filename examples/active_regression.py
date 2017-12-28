2641e825f813ce794e384f2dd31f46dcb50adac9
cosmic-cortex <28926195+cosmic-cortex@users.noreply.github.com>
2017-12-28 20:33:25 +0100

    examples/active_regression.py implemented

---
+++ examples/active_regression.py
@@ -0,0 +1,60 @@
+"""
+Active regression example with Gaussian processes.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import WhiteKernel, RBF
+from modAL.models import ActiveLearner
+
+
+# defining the query strategy for regression
+def GP_regression_std(regressor, X):
+    _, std = regressor.predict(X, return_std=True)
+    query_idx = np.argmax(std)
+    return query_idx, X[query_idx]
+
+
+X = np.random.choice(np.linspace(0, 20, 10000), size=200, replace=False).reshape(-1, 1)
+y = np.sin(X) + np.random.normal(scale=0.3, size=X.shape)
+
+n_initial = 3
+initial_idx = np.random.choice(range(len(X)), size=n_initial, replace=False)
+X_initial, y_initial = X[initial_idx], y[initial_idx]
+
+kernel = RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e3)) \
+         + WhiteKernel(noise_level=1, noise_level_bounds=(1e-10, 1e+1))
+regressor = ActiveLearner(
+    predictor=GaussianProcessRegressor(kernel=kernel),
+    query_strategy=GP_regression_std,
+    X_initial=X_initial.reshape(-1, 1), y_initial=y_initial.reshape(-1, 1)
+)
+
+# plotting the initial estimation
+with plt.style.context('seaborn-white'):
+    plt.figure(figsize=(30, 15))
+    x = np.linspace(0, 20, 1000)
+    pred, std = regressor.predict(x.reshape(-1,1), return_std=True)
+    plt.plot(x, pred)
+    plt.fill_between(x, pred.reshape(-1, )-std, pred.reshape(-1, )+std, alpha=0.2)
+    plt.scatter(X, y, c='k')
+    plt.title('Initial estimation based on %d points' % n_initial)
+    plt.show()
+
+n_queries = 10
+for idx in range(n_queries):
+    query_idx, query_instance = regressor.query(X)
+    print(query_idx)
+    regressor.teach(X[query_idx].reshape(1, -1), y[query_idx].reshape(1, -1))
+
+# plotting after active learning
+with plt.style.context('seaborn-white'):
+    plt.figure(figsize=(30, 15))
+    x = np.linspace(0, 20, 1000)
+    pred, std = regressor.predict(x.reshape(-1,1), return_std=True)
+    plt.plot(x, pred)
+    plt.fill_between(x, pred.reshape(-1, )-std, pred.reshape(-1, )+std, alpha=0.2)
+    plt.scatter(X, y, c='k')
+    plt.title('Estimation after %d queries' % n_queries)
+    plt.show()
