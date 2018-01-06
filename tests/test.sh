acede55dd4c4e88cf5e8ae0a957e656a0eace1e2
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2018-01-06 14:59:16 +0100

    redundant tests removed

--- tests/test.sh
+++ tests/test.sh
@@ -10,16 +10,3 @@ echo core tests failed
 exit 1
 fi
 
-for example_test in tests/example_tests/*
-do
-  echo executing $example_test ...
-  python3 $example_test
-  exit_code=$?
-  if [ $exit_code -eq 0 ]; then
-  echo $example_test successfully executed
-  else
-  echo $example_test failed
-  exit 1
-  fi
-done
-
