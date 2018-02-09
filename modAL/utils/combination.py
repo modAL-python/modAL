10999a137742faf676f6bc9221d362d67623c683
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2018-02-09 13:31:31 +0100

    modAL.utils.combination added for linear combination and product of functions

---
+++ modAL/utils/combination.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def make_linear_combination(*functions, weights=None):
+    if weights is None:
+        weights = np.ones(shape=(len(functions)))
+    else:
+        assert len(functions) == len(weights), 'the length of weights must be the ' \
+                                               'same as the number of given functions'
+
+    def linear_combination(*args, **kwargs):
+        return np.sum([weights[i]*functions[i](*args, **kwargs)
+                       for i in range(len(weights))], axis=0)
+
+    return linear_combination
+
+
+def make_product(*functions, exponents=None):
+    if exponents is None:
+        exponents = np.ones(shape=(len(functions)))
+    else:
+        assert len(functions) == len(exponents), 'the length of exponents must be the ' \
+                                                 'same as the number of given functions'
+
+    def product_function(*args, **kwargs):
+        return np.product([exponents[i]*functions[i](*args, **kwargs)
+                       for i in range(len(exponents))], axis=0)
+
+    return product_function
