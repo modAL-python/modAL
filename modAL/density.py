30ff4509b76e13b53cf0592cbe5b763e0230f17d
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2018-02-09 10:39:20 +0100

    Information density measures added

---
+++ modAL/density.py
@@ -0,0 +1,27 @@
+"""
+============================
+Information density measures
+============================
+-----------------------------------------------------------------
+Measures for estimating the information density of a given sample
+-----------------------------------------------------------------
+"""
+
+import numpy as np
+from scipy.spatial.distance import cosine
+
+
+def similarize_distance(distance_measure):
+
+    def sim(x, y):
+        return 1/(1 + distance_measure(x, y))
+
+    return sim
+
+
+def similarity(X_pool, similarity_measure=similarize_distance(cosine)):
+    sim = np.zeros(shape=(len(X_pool), ))
+    for X_idx, X in enumerate(X_pool):
+        sim[X_idx] = sum(similarity_measure(X, X_j) for X_j in X_pool)
+
+    return sim/len(X_pool)
