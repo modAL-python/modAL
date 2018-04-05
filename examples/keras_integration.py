a9f70525008f81b193c94752d058f962c973c74d
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2018-04-05 10:08:39 +0200

    examples/keras_integration.py input image flattening removed

--- examples/keras_integration.py
+++ examples/keras_integration.py
@@ -7,7 +7,7 @@ import keras
 import numpy as np
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras.wrappers.scikit_learn import KerasClassifier
 from modAL.models import ActiveLearner
 
@@ -18,13 +18,30 @@ def create_keras_model():
     This function compiles and returns a Keras model.
     Should be passed to KerasClassifier in the Keras scikit-learn API.
     """
+
     model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(
+        loss=keras.losses.categorical_crossentropy,
+        optimizer=keras.optimizers.Adadelta(),
+        metrics=['accuracy']
+    )
+
+    """model = Sequential()
     model.add(Dense(512, activation='relu', input_shape=(784, )))
     model.add(Dropout(0.2))
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.2))
     model.add(Dense(10, activation='sigmoid'))
-    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])"""
 
     return model
 
@@ -41,8 +58,8 @@ Data wrangling
 
 # read training data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-X_train = X_train.reshape(60000, 784).astype('float32') / 255
-X_test = X_test.reshape(10000, 784).astype('float32') / 255
+X_train = X_train.reshape(60000, 28, 28, 1).astype('float32') / 255
+X_test = X_test.reshape(10000, 28, 28, 1).astype('float32') / 255
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
 
@@ -65,20 +82,21 @@ Training the ActiveLearner
 learner = ActiveLearner(
     estimator=classifier,
     X_training=X_initial, y_training=y_initial,
-    verbose=0
+    verbose=1
 )
 
 # the active learning loop
 n_queries = 10
 for idx in range(n_queries):
     query_idx, query_instance = learner.query(X_pool, n_instances=200, verbose=0)
+    print(query_idx)
     learner.teach(
         X=X_pool[query_idx], y=y_pool[query_idx],
-        verbose=0
+        verbose=1
     )
     # remove queried instance from pool
     X_pool = np.delete(X_pool, query_idx, axis=0)
     y_pool = np.delete(y_pool, query_idx, axis=0)
 
 # the final accuracy score
-print(learner.score(X_test, y_test, verbose=0))
+print(learner.score(X_test, y_test, verbose=1))
