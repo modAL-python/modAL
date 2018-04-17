352ff57990702ee182f74efa8539ffb97721d762
cosmic-cortex <cosmic-cortex@users.noreply.github.com>
2018-04-17 14:38:20 +0200

    modAL.cluster methods return NotImplemented

--- modAL/cluster.py
+++ modAL/cluster.py
@@ -4,18 +4,15 @@ Cluster-based active learning algorithms
 ========================================
 """
 
-import numpy as np
-from sklearn.cluster.hierarchical import AgglomerativeClustering, _hc_cut
+from sklearn.cluster.hierarchical import AgglomerativeClustering
 
 
 class HierarchicalClustering:
-    def __init__(self, X, classes, n_batch=1):
-        self.classes = classes
-        self.cluster = AgglomerativeClustering(compute_full_tree=True)
-        self.cluster.fit(X)
+    def __init__(self):
+        return NotImplemented
 
     def __call__(self, *args, **kwargs):
-        pass
+        return NotImplemented
 
     def compute_errors(self):
-        pass
+        return NotImplemented
