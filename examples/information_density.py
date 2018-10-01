9f4ad6704e5079da956ee190c6b62837fd2abbab
cosmic-cortex <theodore.danka@gmail.com>
2018-10-01 13:30:29 +0200

    fix: examples updated

--- examples/information_density.py
+++ examples/information_density.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt
 
-from modAL.density import similarize_distance, information_density
+from modAL.density import information_density
 from sklearn.datasets import make_blobs
-from scipy.spatial.distance import euclidean
 
 X, y = make_blobs(n_features=2, n_samples=1000, centers=3, random_state=0, cluster_std=0.7)
 
-cosine_density = information_density(X)
-euclidean_density = information_density(X, similarize_distance(euclidean))
+cosine_density = information_density(X, 'cosine')
+euclidean_density = information_density(X, 'euclidean')
 
 # visualizing the cosine and euclidean information density
 with plt.style.context('seaborn-white'):
