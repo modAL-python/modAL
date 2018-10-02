1cc2f1dd0c37ae23e51a4e7fcce3b699cecec863
cosmic-cortex <theodore.danka@gmail.com>
2018-10-02 18:08:43 +0200

    update: setup script updated

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='modAL',
-    version='0.3.0',
+    version='0.3.1',
     author='Tivadar Danka',
     author_email='85a5187a@opayq.com',
     description='A modular active learning framework for Python3',
     license='MIT',
     url='https://cosmic-cortex.github.io/modAL',
-    packages=['modAL', 'modAL.utils'],
+    packages=['modAL', 'modAL.models', 'modAL.utils'],
     classifiers=['Development Status :: 4 - Beta'],
     install_requires=['numpy>=1.13', 'scikit-learn>=0.18', 'scipy>=0.18'],
 )
