8cbaea99942aacee4ce09dd5215963cc7bdb1139
cosmic-cortex <theodore.danka@gmail.com>
2020-11-01 09:39:01 +0100

    version number bumped

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='modAL',
-    version='0.3.6',
+    version='0.4.0',
     author='Tivadar Danka',
     author_email='85a5187a@opayq.com',
     description='A modular active learning framework for Python3',
